ce12cd752306f7e17d2c99e8d52b97b107b0fc94
Sandro Simperl <sandrosimperl.cc@gmail.com>
2022-11-29 09:25:28 +0100

    formatiert

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -1,45 +1,61 @@
 package com.cc.java;
-
 import java.util.ArrayList;
 
 public class App {
 
-	public static void main(String[] args) {
-		
+	public static void main( String[] args ) {
 		// Vorüberlegung
 		// Cat cat = new Cat("Grizabella");
-		// // output(cat.getAttributes());
+		// // out(cat.getAttributes());
 		
 		//cat1 / cat2 .. more of the same ...DRY
-		// for (int i = 0; i < 10; i++) 
-		// {
-		// 	new Cat("someone_" + i); // Objekt, aber KEINE Referenz!
-		// }
+		for (int i = 0; i < 10; i++) 
+		{
+			new Cat("someone_" + i); // Objekt, aber KEINE Referenz!
+		}
 		
-		String [] nameArr = {"Grizabella","Alonzo", "RumTumTugger","Suzanne"};
+		String [] nameArr = { "Grizabella", "Alonzo", "RumTumTugger", "Suzanne" };
 		ArrayList<Cat> objList = new ArrayList<>();
 
-		for (int i = 0; i < nameArr.length; i++) 
-		// for (int i = 0; i < 4; i++) 
-		{
-			objList.add(new Cat(nameArr[i]));
-			output("Cats: " + objList);
-
-			objList.add(new Cat("someone_" + i));
-		
-
+		out( c( "#y" ) + "---Cats Array:-----------------------------" );
+		for ( int i = 0; i < nameArr.length - 1; i++ ){
+			objList.add( new Cat( nameArr[ i ] ) );
+			objList.add( new Cat( "someone_" + i ) );
+			out( t( 1 ) + c( "#r" ) +  "Cats: " + c( "#g" ) +  objList );
 		}
 
-		output("Cats: " + objList);
-		output(objList.get(0).getAttributes());
-		output(objList.get(1).getAttributes());
-		output(objList.get(objList.size()-1).getAttributes());
-		
+		out( c( "#y" ) + "---Cats:-----------------------------------" );
+		out( t( 1 ) + c( "#g" ) + objList );
 
+		out( "\n" + c( "#y" ) + "---getAttributes:--------------------------" );
+		out( t( 1 ) + c( "#g" ) +  objList.get( 0 ).getAttributes() );
+		out( t( 1 ) + c( "#g" ) +  objList.get( 1 ).getAttributes() );
+		out( t( 1 ) + c( "#g" ) +  objList.get( objList.size() - 1 ).getAttributes() );
 	}
 	
-	public static void output(String outStr) {
-		System.out.println(outStr);
+	// out
+	public static void out( String o ) { System.out.println( o ); }
+	// colored out
+	public static String c( String c ) {
+		switch ( c ) {
+			case "#r": return "\u001b[31m";
+			case "#g": return "\u001b[32m";
+			case "#y": return "\u001b[33m";
+			case "#b": return "\u001b[34m";
+			case "#p": return "\u001b[35m";
+			case "#c": return "\u001b[36m";
+			default: return "\u001b[0m";
+		}
 	}
+	// tab controler
+	public static String t( int t ) {
+        switch ( t ) {
+            case 1: return "\t";
+            case 2: return "\t";
+            case 3: return "\t\t\t";
+            case 4: return "\t\t\t\t";
+            default: return "";
+        }
+    }
 
 }
--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -1,18 +1,12 @@
 package com.cc.java;
 
 public class Cat {
-
 	private String name;
-
-	
 	public Cat(String name) {
 		this.name = name;
-		System.out.println(getAttributes()); 
+		// System.out.println( App.t( 1 ) + App.c( "#g" ) +  getAttributes() ); 
 	}
-	
 	public String getAttributes() {
 		return "Hi, I'm " + name + " and I live @: " + this;
 	}
-	
-	
 }
